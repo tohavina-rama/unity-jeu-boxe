34c79e5befc294d9d845e0035badf9a80a9220a7
Sebcoach13 <140277344+Sebcoach13@users.noreply.github.com>
2025-06-16 00:16:35 +0200

    playgame

--- Assets/Scripts/GameAccueilScript.cs
+++ Assets/Scripts/GameAccueilScript.cs
@@ -1,14 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayGameScript : MonoBehaviour
+public class GameAccueilScript: MonoBehaviour
 {
     public void LoadGameScene()
     {
         SceneManager.LoadScene("salleDeBoxe");
     }
 }
-
-
-
-
